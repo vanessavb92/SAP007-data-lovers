0bec48d403b8451fc6ccda3c1f12012b2cb1b870
vanessa borges <vanessacamahuali28@gmail.com>
2022-02-07 15:42:39 -0400

    novo arquivo index2.js

---
+++ src/index2.js
@@ -0,0 +1,32 @@
+document.querySelector('.card-number-input').oninput = () =>{
+    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
+}
+
+document.querySelector('.card-holder-input').oninput = () =>{
+    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
+}
+
+document.querySelector('.mes-input').oninput = () =>{
+    document.querySelector('.exp-mes').innerText = document.querySelector('.mes-input').value;
+}
+
+document.querySelector('.ano-input').oninput = () =>{
+    document.querySelector('.val-ano').innerText = document.querySelector('.ano-input').value;
+}
+
+document.querySelector('.cvv-input').onmouseenter = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+
+document.querySelector('.cvv-input').onmouseleave = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+}
+
+document.querySelector('.cvv-input').oninput = () =>{
+    document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
+}
+
+
+
