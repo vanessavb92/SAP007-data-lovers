e14cf7a3344f766262b92234dc72d85f39254916
vanessa borges <vanessacamahuali28@gmail.com>
2022-02-10 08:53:35 -0400

    corrigindo erros

--- src/index2.js
+++ src/index2.js
@@ -1,34 +1,31 @@
 import validator from "./validator.js";
 
-let cardNumber = document.getElementById("cardNumber");
-
+const cardNumber = document.getElementById("cardNumber");
 const validate = document.getElementById("buttonValidate");
+const validationAnswer = document.getElementById("validationAnswer");
+const cardNumberMask = document.getElementById("maskNumber");
 
 validate.addEventListener("click", function (e) {
-  e.preventDefault();
+    e.preventDefault();
+    console.log("clicou")
+    let cardNumberValue = cardNumber.value.replace(/\D/g, " ");
 
-  let cardNumberValue = cardNumber.value.replace(/\D/g, " ");
 
-  const validationAnswer = document.getElementById("validationAnswer");
-  const cardNumberMask = document.getElementById("maskNumber");
+    const result = validator.isValid(cardNumberValue);
+    if (result === true) {
+        validationAnswer.style.color = "#149F50";
+        validationAnswer.style.fontWeight = "600";
+        validationAnswer.textContent = "CARTÃO VÁLIDO";
+    } else {
+        validationAnswer.style.color = "#FF0F0F";
+        validationAnswer.style.fontWeight = "600";
+        validationAnswer.textContent = "CARTÃO INVÁLIDO";
+    }
 
-  const result = validator.isValid(cardNumberValue);
-
-  if (result === true) {
-    validationAnswer.style.color = "#149F50";
-    validationAnswer.style.fontWeight = "600";
-    validationAnswer.textContent = "CARTÃO VÁLIDO";
-  } else {
-    validationAnswer.style.color = "#FF0F0F";
-    validationAnswer.style.fontWeight = "600";
-    validationAnswer.textContent = "CARTÃO INVÁLIDO";
-  }
-
-  let mascara = validator.maskify(cardNumberValue);
-
-  if (result === true) {
-    cardNumberMask.innerHTML = mascara;
-  } else {
-    cardNumberMask.innerHTML = "Tente de novo";
-  }
+    let mascara = validator.maskify(cardNumberValue);
+    if (result === true) {
+        cardNumberMask.innerHTML = mascara;
+    } else {
+        cardNumberMask.innerHTML = "Tente novamente";
+    }
 });
