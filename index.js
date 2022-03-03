3e2383f1aafb0eb72d5b51d064b021061e528e51
vanessa borges <vanessacamahuali28@gmail.com>
2022-03-03 17:50:20 -0300

    revisão

--- index.js
+++
@@ -1,5 +0,0 @@
-import validator from './validator.js';
-userName = document.getElementById('name').value;
-document.getElementById('typedName').innerHTML = "Bem vinda: " + userName;
-
-console.log(validator);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-
-document.getElementById("Carrinho").addEventListener("click", Pagina);
-
-function Pagina(){
-    window.location.href="cafe.html"
-}
